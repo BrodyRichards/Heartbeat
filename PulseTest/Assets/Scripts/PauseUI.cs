9902e3858d0c4af0f5cda3bd2ffecbdbfd417eec
shuoen1997 <sli112@ucsc.edu>
2019-01-22 13:26:56 -0800

    Add menu buttons

---
+++ PulseTest/Assets/Scripts/PauseUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    // thanks to Brackey's tutorial
+    // https://www.youtube.com/watch?v=JivuXdrIHK0
+
+    public static bool IsPaused = false;
+    public GameObject pauseUI;
+
+    private void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+}
