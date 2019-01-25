7703d0b3a09fb41f5cb63bd23f430bad79057b30
JustinP <jfpiment@ucsc.edu>
2019-01-24 22:11:46 -0800

    Characters are now clickable
    Character switching is now activated by clicking on the desired character.

--- PulseTest/Assets/Scripts/characterSwitcher.cs
+++ PulseTest/Assets/Scripts/characterSwitcher.cs
@@ -21,7 +21,7 @@ public class characterSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Poll for key press
+        //Poll for mouse click
         switchCharacter();
     }
 
@@ -29,21 +29,28 @@ public class characterSwitcher : MonoBehaviour
     {
         return charChoice;
     }
+
     //Function to handle character switching when 'E' is pressed
     private void switchCharacter()
     {
-        //Looking for 'E' to be pressed
-        if (Input.GetKeyDown(KeyCode.E))
+        //Looking for 'Left Mouse Button' to be pressed
+        if (Input.GetMouseButtonDown(0))
         {
-            //Cycle through choices on key press
-            if (charChoice < 3)
+            //Vector for Raycast, takes mouse position
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Decompose to 2D vector
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            //Raycast hit register for mouse position
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            //If a hit is registered, find which object was hit
+            if (hit.collider != null)
             {
-                charChoice++;
-            }
-            else
-            {
-                charChoice = 0;
-            }
+                //Take the name of the object and convert to int for charChoice
+                string name = hit.collider.gameObject.name;
+                int.TryParse(name, out charChoice);
+            }       
 
             //Activate the object chosen and disable all the others
             GameObject choice = findGO(charChoice);
@@ -69,7 +76,7 @@ public class characterSwitcher : MonoBehaviour
     //Helper function for finding game objects
     private GameObject findGO(int i)
     {
-        string choice = "bush" + i.ToString();
+        string choice = i.ToString();
         GameObject someB = GameObject.Find(choice);
         return someB;
     }
